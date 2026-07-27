c6c5cd2961aed0687a8a12ec3fa89640db79939f
b250430-mahak <b250430@skit.ac.in>
2026-07-28 01:00:38 +0530

    Update flash.php

--- includes/flash.php
+++ includes/flash.php
@@ -1,18 +1,13 @@
 <?php
-/*
-    Flash Message Helper
-    ----------------------
-    Small helper functions to set and show one-time success/error
-    messages after actions like add, edit, delete, login, etc.
-*/
 
-// Call this before redirecting to store a message for the next page
+
+
 function set_flash($type, $message) {
-    // $type should be 'success' or 'error'
+    
     $_SESSION['flash_' . $type] = $message;
 }
 
-// Call this on the page that should display the message
+
 function show_flash() {
     if (!empty($_SESSION['flash_success'])) {
         echo '<div class="alert alert-success">' . htmlspecialchars($_SESSION['flash_success']) . '</div>';
