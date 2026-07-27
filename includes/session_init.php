d3ca0b93a495ccc7c6f36e2abb4b1200200d9f28
b250430-mahak <b250430@skit.ac.in>
2026-07-28 01:02:05 +0530

    Update session_init.php

--- includes/session_init.php
+++ includes/session_init.php
@@ -1,9 +1,5 @@
 <?php
-/*
-    Starts the PHP session if it is not already started.
-    This file is included at the top of every page that
-    needs to read or write session data.
-*/
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
