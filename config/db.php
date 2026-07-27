cd447633dd57a43254883387e012c08b49cd0266
b250430-mahak <b250430@skit.ac.in>
2026-07-28 01:25:41 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,19 +1,13 @@
 <?php
+$host     = "mysql-30e07e41-todo-app123.h.aivencloud.com";
+$port     = 20096;
+$user     = "avnadmin";
+$password = "AVNS_D9rniLXb-ZJ3fotim-D";
+$dbname   = "defaultdb";
 
-
-$host    = "localhost";   
-$db_user = "root";        
-$db_pass = "";            
-$db_name = "todo_db";      
-
-
-$conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
-
+$conn = mysqli_connect($host, $user, $password, $dbname, $port);
 
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Database Connection Failed: " . mysqli_connect_error());
 }
-
-
-mysqli_set_charset($conn, "utf8mb4");
 ?>
